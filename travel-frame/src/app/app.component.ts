5b1fa32aeb67c2ee129d9cf3bcbbce43f4468b6c
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-06 10:18:44 +0200

    add

--- travel-frame/src/app/app.component.ts
+++ travel-frame/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HomeComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
