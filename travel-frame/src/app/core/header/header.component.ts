72916fae2eaeb7183bc0ea0284c17037efb0fe09
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-06 15:15:07 +0200

    add

--- travel-frame/src/app/core/header/header.component.ts
+++ travel-frame/src/app/core/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ImagesHeroRowsComponent } from './images-hero-rows/images-hero-rows.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,ImagesHeroRowsComponent],
+  imports: [CommonModule,ImagesHeroRowsComponent,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
