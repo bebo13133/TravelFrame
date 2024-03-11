e54c96ff50286074a7639fdd4cabbcd74c51240e
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-11 17:13:42 +0200

    add

--- travel-frame/src/app/create-page/create-page.component.ts
+++ travel-frame/src/app/create-page/create-page.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-page',
@@ -15,6 +15,10 @@ export class CreatePageComponent implements OnInit {
   currentFormSection = 1; 
   totalFormSections = 3;
   imagesPreview: string[] = []; 
+  dayImages: { previews: string[] }[] = [];
+  currentDayIndex: number = 0; // Стартирайте от първия ден
+
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -24,9 +28,51 @@ export class CreatePageComponent implements OnInit {
       paragraph: ['', Validators.required],
       "title-desc": ['', Validators.required],
       'info-desc': ['', Validators.required],
+      'images': [null] ,
+      days: this.fb.array([this.initDayForm()]),
+      });
+      this.dayImages.push({ previews: [] });
+  }
+  initDayForm(): FormGroup {
+    return this.fb.group({
+      dayImage: [null],
+      dayTitle: ['', Validators.required],
+      dayInfo: ['', Validators.required]
     });
+  };
+  goToDay(index: number) {
+    this.currentDayIndex = index;
+  }
+  // Функция за добавяне на нов ден
+  addDay() {
+    const days = this.createForm.get('days') as FormArray;
+    days.push(this.initDayForm());
+    this.dayImages.push({ previews: [] });
+  };
+
+  // Функция за премахване на ден
+  removeDay(index: number) {
+    const days = this.createForm.get('days') as FormArray;
+    if (days.length > 1) {
+      days.removeAt(index);
+      this.dayImages.splice(index, 1); // Премахваме снимките за деня
+    }
+  };
+  nextDay() {
+    if (this.currentDayIndex < this.days.length - 1) {
+      this.currentDayIndex++;
+    }
   }
 
+  previousDay() {
+    if (this.currentDayIndex > 0) {
+      this.currentDayIndex--;
+    }
+  }
+    // Функция за получаване на дните като FormArray
+    get days(): FormArray {
+      return this.createForm.get('days') as FormArray;
+    }
   onImageChange(event: Event) {
     const file = (event.target as HTMLInputElement).files?.[0];
     if (file) {
@@ -52,8 +98,21 @@ export class CreatePageComponent implements OnInit {
 
       console.error("Можете да качите максимум 4 снимки.");
     }
+  };
+  onDayImageChange(event: Event, dayIndex: number) {
+    const files = (event.target as HTMLInputElement).files;
+    if (files) {
+      this.dayImages[dayIndex].previews = []; // Изчистваме текущите превюта за деня
+      Array.from(files).forEach(file => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          // Добавяме превюто към съответния ден
+          this.dayImages[dayIndex].previews.push(reader.result as string);
+        };
+        reader.readAsDataURL(file);
+      });
+    }
   }
-
   onSubmit() {
    
     console.log(this.createForm.value);
