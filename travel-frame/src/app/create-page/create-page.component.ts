1c64567c4bc8086eb656fc3e7d1fb1818c3dedce
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-15 13:54:39 +0200

    add

--- travel-frame/src/app/create-page/create-page.component.ts
+++ travel-frame/src/app/create-page/create-page.component.ts
@@ -55,9 +55,9 @@ export class CreatePageComponent implements OnInit {
       days: this.fb.array([this.initDayForm()]),
 
     });
-    this.dayImages.push({ previews: [] });
-    console.log('fs', this.createForm.value)
+    // console.log('fs', this.createForm.value)
   }
+
   initDayForm(): FormGroup {
     return this.fb.group({
       dayImage: [null],
@@ -69,22 +69,51 @@ export class CreatePageComponent implements OnInit {
     this.currentDayIndex = index;
   }
 
-  addDay() {
-    const days = this.createForm.get('days') as FormArray;
-    const newDayFormGroup = this.initDayForm();
-    days.push(newDayFormGroup);
 
-    this.currentDayIndex = days.length - 1;
-  };
+//   addDay() {
+//     const days = this.createForm.get('days') as FormArray;
+//     const newDayFormGroup = this.initDayForm();
+//     days.push(newDayFormGroup);
+  
+//     // Добавяне на нов обект в dayImages масива за новия ден
+//     this.dayImages.push({ previews: [] });
+  
+//     this.currentDayIndex = days.length - 1;
+//   }
+// // доплънителна проверк за темплейта
+// hasPreviewsForCurrentDay(): boolean {
+//   return this.dayImages[this.currentDayIndex]?.previews?.length > 0 ?? false;
+// }
 
+//   removeDay(index: number) {
+//     const days = this.createForm.get('days') as FormArray;
+//     if (days.length > 1) {
+//       days.removeAt(index);
+//       this.dayImages.splice(index, 1);
+//     }
+//   };
+  // onDayImageChange(event: Event, dayIndex: number) {
+  //   const files = (event.target as HTMLInputElement).files;
+  //   if (files) {
+  //     // Проверка дали съществува обект за текущия индекс и инициализация, ако не
+  //     if (!this.dayImages[dayIndex]) {
+  //       this.dayImages[dayIndex] = { previews: [] };
+  //     }
+      
+  //     // Изчистваме текущите превюта за деня
+  //     this.dayImages[dayIndex].previews = [];
+      
+  //     // Обработка на всеки файл и добавяне към превюта
+  //     Array.from(files).forEach(file => {
+  //       const reader = new FileReader();
+  //       reader.onload = () => {
+  //         this.dayImages[dayIndex].previews.push(reader.result as string);
+  //       };
+  //       reader.readAsDataURL(file);
+  //     });
+  //   }
+  // }
 
-  removeDay(index: number) {
-    const days = this.createForm.get('days') as FormArray;
-    if (days.length > 1) {
-      days.removeAt(index);
-      this.dayImages.splice(index, 1);
-    }
-  };
   nextDay() {
     if (this.currentDayIndex < this.days.length - 1) {
       this.currentDayIndex++;
@@ -127,21 +156,88 @@ export class CreatePageComponent implements OnInit {
       console.error("Можете да качите максимум 4 снимки.");
     }
   };
-  onDayImageChange(event: Event, dayIndex: number) {
+
+
+  addDay() {
+    const days = this.createForm.get('days') as FormArray;
+    days.push(this.initDayForm());
+    this.dayImages.push({ previews: [] });
+  }
+  
+  removeDay(index: number) {
+    const days = this.createForm.get('days') as FormArray;
+    days.removeAt(index);
+    this.dayImages.splice(index, 1);
+  }
+  
+  onDayImageChange(event: Event, index: number) {
     const files = (event.target as HTMLInputElement).files;
     if (files) {
-      this.dayImages[dayIndex].previews = [];
+      this.dayImages[index] = { previews: [] };
       Array.from(files).forEach(file => {
         const reader = new FileReader();
         reader.onload = () => {
-
-          this.dayImages[dayIndex].previews.push(reader.result as string);
+          this.dayImages[index].previews.push(reader.result as string);
         };
         reader.readAsDataURL(file);
       });
     }
   }
+  getDayImagesPreviews(index: number): string[] {
+    return this.dayImages[index]?.previews ?? [];
+  }
+
+
+
+
+
+
+
+
+  
+  goToNextSection() {
+    if (this.currentFormSection < this.totalFormSections) {
+      this.currentFormSection++;
+    }
+
+  }
+
+  goToPreviousSection() {
+    if (this.currentFormSection > 1) {
+      this.currentFormSection--;
+    }
+
+  }
   onSubmit() {
+
+    // prowerki
+
+        // console.log('status',this.createForm.status); // Покажете общия статус на формата
+    // console.log('formValue',this.createForm.value); // Покажете стойностите на формата
+    // console.log('errors',this.createForm.errors); // Покажете грешките на формата, ако има такива
+  
+    // if (this.createForm.invalid) {
+    //   console.log('controles',this.createForm.controls); // Ще покаже статуса на всички контроли
+    // }
+    // const days = this.createForm.get('days') as FormArray;
+    // let isValid = true;
+  
+    // days.controls.forEach((dayFormGroup, index) => {
+    //   if (!dayFormGroup.valid) {
+    //     console.log(`Day ${index + 1} is not valid.`);
+    //     isValid = false;
+    //   }
+    // });
+  
+    // if (!isValid) {
+    //   console.log('There are invalid days. Cannot submit.');
+    //   return;
+    // }
+
+
+
+
+
 
     if (this.createForm.valid) {
       const formData = {
@@ -175,17 +271,4 @@ export class CreatePageComponent implements OnInit {
     }
   }
 
-  goToNextSection() {
-    if (this.currentFormSection < this.totalFormSections) {
-      this.currentFormSection++;
-    }
-
-  }
-
-  goToPreviousSection() {
-    if (this.currentFormSection > 1) {
-      this.currentFormSection--;
-    }
-
-  }
 }
