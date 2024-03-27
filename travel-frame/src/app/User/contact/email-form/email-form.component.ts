9e67e72f240c70a2d307763d2b8aa83aac2c3f82
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-27 12:18:11 +0200

    add emailjs

--- travel-frame/src/app/User/contact/email-form/email-form.component.ts
+++ travel-frame/src/app/User/contact/email-form/email-form.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-email-form',
@@ -11,27 +12,30 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './email-form.component.html',
   styleUrl: './email-form.component.css'
 })
-export class EmailFormComponent {
-  contactForm!: FormGroup<any>;
+export class EmailFormComponent implements OnInit {
+  contactForm!: FormGroup;
 
-    constructor(private fb:FormBuilder){}
-
-  ngOnInit(): void {
-    this.contactForm = this.fb.group({
-      userName: ['', Validators.required],
-      userEmail: ['', [Validators.required, Validators.email]],
-      userSubject: ['', Validators.required],
-      message: ['', Validators.required]
-    });
-  }
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      // Изпращане на формата
-      console.log(this.contactForm.value);
-    } else {
-      // Показване на съобщение за грешка
-      console.log('Form is not valid');
+  constructor(private fb: FormBuilder) {}
+    ngOnInit(): void {
+      this.contactForm = this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        subject: ['', Validators.required],
+        message: ['', Validators.required]
+      });
     }
+    sendEmail(e: Event): void {
+      e.preventDefault();
+  
+        const formData = e.target as HTMLFormElement;
+  console.log(formData)
+        emailjs.sendForm('service_zxhuqbx', 'template_7tkpsx5', formData, 'iRYFR4BuAXZEBF1ld')
+          .then((response: any) => {
+            console.log('Email sent successfully:', response);
+            formData.reset();
+          }, (error: any) => {
+            console.error('Error sending email:', error);
+          });
+     
   }
 }
--- travel-frame/src/app/core/footer/footer.component.ts
+++ travel-frame/src/app/core/footer/footer.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
