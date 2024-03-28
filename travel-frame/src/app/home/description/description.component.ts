0d656ebc9eb3ac19c2ba0b5c154202060c9374a4
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2024-03-28 10:32:34 +0200

    add

--- travel-frame/src/app/home/description/description.component.ts
+++ travel-frame/src/app/home/description/description.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-description',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './description.component.html',
   styleUrl: './description.component.css'
 })
